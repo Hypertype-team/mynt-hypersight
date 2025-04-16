240ea9067dfdcf634623748dec100ebea865f1fd
Manuel Fraile <manuelfraile@Manuels-MacBook-Pro.local>
2025-04-16 13:57:04 +0200

    Remove chat

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Sidebar } from "./Sidebar";
 export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen overflow-hidden bg-background">
-      <Sidebar />
+      {/* <Sidebar /> */}
       <main className="flex-1 overflow-auto px-6 py-6">
         {children}
       </main>
